01c181af5b0ba1bebc56215e2a27c7b745fb9f02
Mica CF <mcohenfuentes@gmail.com>
2025-04-02 12:38:25 -0400

    added crud

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Create your views here.
-from django.http import HttpResponse
 
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
 def home(request):
@@ -29,3 +15,19 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
